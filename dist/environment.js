be67db600e9e005931920a7d811efadbc7b0b518
Arifcodes11 <arifcodes11@gmail.com>
2025-08-08 18:13:33 +0530

    feat: Implement likes functionality with routes and controllers
    - Added likes-routes.js to handle like-related endpoints.
    - Created likes-controllers.js for managing like operations.
    - Defined likes-types.js for error handling related to likes.
    - Introduced session-middleware.js for authentication checks.
    - Updated posts-routes.js to integrate likes functionality.
    - Created posts-controllers.js for managing post-related operations.
    - Added posts-types.js for error handling related to posts.
    - Implemented users-routes.js and users-controllers.js for user management.
    - Defined users-types.js for error handling related to users.
    - Created routes-index.js to consolidate all routes and enable CORS.

---
+++ dist/environment.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.betterAuthSecret = exports.webClientUrl = exports.serverUrl = void 0;
+exports.serverUrl = process.env.SERVER_URL || process.exit(1);
+exports.webClientUrl = process.env.WEB_CLIENT_URL || process.exit(1);
+exports.betterAuthSecret = process.env.BETTER_AUTH_SECRET || process.exit(1);
